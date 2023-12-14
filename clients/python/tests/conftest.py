ef976e4e1de1e1f5a6534ea33487cf681ceefe70
tarilabs <matteo.mortari@gmail.com>
2023-12-14 09:25:44 +0100

    remove fixture autouse

--- clients/python/tests/conftest.py
+++ clients/python/tests/conftest.py
@@ -20,7 +20,7 @@ ProtoTypeType = Union[ArtifactType, ContextType]
 
 # ruff: noqa: PT021 supported
 @pytest.fixture(scope="session")
-def plain_wrapper(request) -> MLMDStore:
+def mlmd_conn(request) -> MetadataStoreClientConfig:
     model_registry_root_dir = model_registry_root(request)
     print("Assuming this is the Model Registry root directory:", model_registry_root_dir)
     shared_volume = model_registry_root_dir / "test/config/ml-metadata"
@@ -36,9 +36,10 @@ def plain_wrapper(request) -> MLMDStore:
     wait_for_logs(container, "Server listening on")
     os.system('docker container ls --format "table {{.ID}}\t{{.Names}}\t{{.Ports}}" -a') # noqa governed test
     print("waited for logs and port")
-    cfg = MetadataStoreClientConfig()
-    cfg.host = "localhost"
-    cfg.port = int(container.get_exposed_port(8080))
+    cfg = MetadataStoreClientConfig(
+        host = "localhost",
+        port = int(container.get_exposed_port(8080))
+    )
     print(cfg)
 
     # this callback is needed in order to perform the container.stop()
@@ -51,18 +52,18 @@ def plain_wrapper(request) -> MLMDStore:
     request.addfinalizer(teardown)
 
     time.sleep(3) # allowing some time for mlmd grpc to fully stabilize (is "spent" once per pytest session anyway)
-    store = metadata_store.MetadataStore(cfg)
-    wait_for_grpc(container, store)
+    _throwaway_store = metadata_store.MetadataStore(cfg)
+    wait_for_grpc(container, _throwaway_store)
 
-    return MLMDStore(cfg)
+    return cfg
 
 
 def model_registry_root(request):
     return (request.config.rootpath / "../..").resolve()  # resolves to absolute path
 
 
-@pytest.fixture(autouse=True)
-def _plain_wrapper_after_each(request, plain_wrapper: MLMDStore):
+@pytest.fixture()
+def plain_wrapper(request, mlmd_conn: MetadataStoreClientConfig) -> MLMDStore:
     sqlite_db_file = model_registry_root(request) / "test/config/ml-metadata/metadata.sqlite.db"
     def teardown():
         try:
@@ -73,6 +74,8 @@ def _plain_wrapper_after_each(request, plain_wrapper: MLMDStore):
         print("plain_wrapper_after_each done.")
 
     request.addfinalizer(teardown)
+
+    return MLMDStore(mlmd_conn)
 
 
 def set_type_attrs(mlmd_obj: ProtoTypeType, name: str, props: list[str]):
